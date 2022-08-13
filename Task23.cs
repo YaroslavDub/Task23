90499b5195b83534af00ee95615bdcfc0de23315
YaroslavDubV <yaroslavduboshin@mail.ru>
2022-08-14 00:24:57 +0300

    Task23

---
+++ Task23.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+double res = 0;
+for (int i = 1; i <= n-1;) {
+    res = Math.Pow(i, 3);
+    Console.Write(res + ", ");
+    i += 1;
+}
+Console.WriteLine(Math.Pow(n, 3));
